ff3b8370a3e664867aab83aa98e250a17b12d844
Alex <a3zhu@uwaterloo.ca>
2023-05-26 23:27:03 -0400

    initialized next js project and replaced boilerplate with green button

---
+++ src/app/components/GreenButton.js
@@ -0,0 +1,8 @@
+import styles from './GreenButton.module.css'
+
+// chat gpt wrote this thing
+export default function GreenButton() {
+  return (
+    <button className={styles.greenbutton}>+</button>
+  );
+};
