4ff3d24a10dc04fe21af0f7a61bd4e92eadd4998
Alex <a3zhu@uwaterloo.ca>
2023-05-29 00:07:31 -0400

    first nested components, CustomAccordionItem working

--- src/app/components/Accordion.js
+++ src/app/components/Accordion.js
@@ -12,11 +12,21 @@ import GreenButton from "./GreenButton.js";
 export default function accordion() {
     return (
         <Accordion>
-            <AccordionItem>
+            <CustomAccordionItem sectionNumber={0} />
+            <CustomAccordionItem sectionNumber={1} />
+            <CustomAccordionItem sectionNumber={2} />
+        </Accordion>
+    );
+}
+
+function CustomAccordionItem(props) {
+    const { sectionNumber } = props;
+    return (
+        <AccordionItem>
                 <h2>
                     <AccordionButton>
                         <Box as="span" flex="1" textAlign="left">
-                            Section 1 title
+                            Section { sectionNumber } title
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
@@ -29,29 +39,5 @@ export default function accordion() {
                     ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </AccordionPanel>
             </AccordionItem>
-
-            <AccordionItem>
-                <h2>
-                    <AccordionButton>
-                        <Box as="span" flex="1" textAlign="left">
-                            Section 2 title
-                        </Box>
-                        <AccordionIcon />
-                    </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </AccordionPanel>
-            </AccordionItem>
-        </Accordion>
-    );
+    )
 }
-
-// function customAccordionItem() {
-//     return (
-        
-//     )
-// }
