bd10d4d50d152da09cda134eccdf34d763fbce5a
Alex <a3zhu@uwaterloo.ca>
2023-06-11 18:58:33 -0400

    added giftgen integration via iframe

--- src/app/components/GiftModal.js
+++ src/app/components/GiftModal.js
@@ -8,13 +8,33 @@ import {
     ModalCloseButton,
     useDisclosure,
     Button,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Box,
+    Tooltip,
+    useToast,
+    Spacer,
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    AlertDialogCloseButton,
+    Icon,
     Text,
-    Icon
+    Textarea,
 } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { FiGift } from "react-icons/fi";
 
 export default function GiftModal() {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const [gifts, setGifts] = useState("");
+    const handleGiftChange = (event) => setGifts(event.target.value);
+
     return (
         <>
             <Button size="lg" rightIcon=<CustomIcon /> onClick={onOpen}>
@@ -24,18 +44,51 @@ export default function GiftModal() {
             <Modal isOpen={isOpen} onClose={onClose} size="full">
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <Text>hello!</Text>
-                    </ModalBody>
+                    <Box ml="40" mr="40" mt="20" mb="20">
+                        <ModalHeader>Gifts</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <FormControl>
+                                <FormLabel>Gift List</FormLabel>
+                                <Input
+                                    placeholder="Gift List"
+                                    value={gifts}
+                                    onChange={handleGiftChange}
+                                />
+                            </FormControl>
+                            <Box>
+                                <FormLabel mt={4}>Idea List</FormLabel>
+                                <Textarea
+                                    size="lg"
+                                    placeholder="Here is a sample placeholder"
+                                />
+                            </Box>
+                            <FormLabel mt={4}>GiftGen</FormLabel>
+                            <div style={{width: '1400px', height: '800px', overflow: 'hidden'}}>
+                                <iframe
+                                    src="https://www.giftgen.co.uk/"
+                                    title="iframe Example 1"
+                                    scrolling="no"
+                                    height = "800px"
+                                    width = "1000px"
+                                    style={{marginLeft: '-60px', marginTop: '-60px'}}
+                                ></iframe>
+                            </div>
+                        </ModalBody>
 
-                    <ModalFooter>
-                        <Button colorScheme="green" mr={3} onClick={onClose}>
-                            Close
-                        </Button>
-                        <Button variant="ghost">Secondary Action</Button>
-                    </ModalFooter>
+                        <ModalFooter>
+                            <Button
+                                colorScheme="green"
+                                mr={3}
+                                onClick={onClose}
+                            >
+                                Update Gifts
+                            </Button>
+                            <Button variant="ghost" onClick={onClose}>
+                                Cancel
+                            </Button>
+                        </ModalFooter>
+                    </Box>
                 </ModalContent>
             </Modal>
         </>
