68efccbb2325b72dcd0a3ecaa07e58ccc68b0e70
Alex <a3zhu@uwaterloo.ca>
2023-06-17 00:20:35 -0400

    added tabs for pending and completed

--- src/app/components/EventList.js
+++ src/app/components/EventList.js
@@ -1,12 +1,37 @@
-import EventItem from "./EventItem.js"
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import EventItem from "./EventItem.js";
 
 export default function EventList({ data }) {
     return (
-        <div>
-            {data.map((item) => (
-                // just add a key for no warning
-                <EventItem key={item?.id} item={item} />
-            ))}
-        </div>
+        <Tabs variant="soft-rounded" colorScheme="orange">
+            <TabList>
+                <Tab>Pending</Tab>
+                <Tab>Completed</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <div>
+                        {data.map((item) => (
+                            // just add a key for no warning
+                            item?.completed == false ?
+                                <EventItem key={item?.id} item={item} />
+                            :
+                            <div></div>
+                        ))}
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <div>
+                        {data.map((item) => (
+                            // just add a key for no warning
+                            item?.completed == true ?
+                                <EventItem key={item?.id} item={item} />
+                            :
+                            <div></div>
+                        ))}
+                    </div>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
     );
 }
